23dc96e7e03f44575901b32baac6ae6c6ffb7db1
Prabin Singh <pravin544@gmail.com>
2020-07-23 20:33:00 -0500

    Added timer in app

--- Assets/codeQuiz.js
+++ Assets/codeQuiz.js
@@ -1,96 +1,91 @@
 var quiz = document.querySelector("#quiz");
 var results = document.querySelector("#results");
 var startquiz = document.querySelector("#startquiz");
-var quesCount =0;
-
-const Questions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "Brendan Eich"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "npm"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "ESLint",
-        b: "jQuery",
-        c: "RequireJS",
-      },
-      correctAnswer: "ESLint"
-    }
-  ];
-
-function buildQuiz(){
-    // setTime();
-    question = Questions[quesCount].question;
-    var content = document.getElementById("content");
-    chA=Questions[quesCount].answers.a;
-    chB=Questions[quesCount].answers.b;
-    chC=Questions[quesCount].answers.c;
-    answer = Questions[quesCount].correctAnswer;
-    content.innerHTML="";
-    content.innerHTML = "<h1>" + Questions[quesCount].question + "<br/></h1>";
-    content.innerHTML += "<div><button id = 'button1' onClick=checkAnswer(this) value='" + chA + "'>" +chA+ "</button></div><br>";
-    content.innerHTML += "<div><button id = 'button2' onClick=checkAnswer(this) value='" + chB + "'>" +chB+"</button></div><br>";
-    content.innerHTML += "<div><button id = 'button3' onClick=checkAnswer(this) value='" + chC + "'>" +chC+"</button></div><br>";
-    quesCount++;
-} 
-
+var quesCount = 0;
 var score = 0;
-function checkAnswer(e){
+const Questions = [{
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "Brendan Eich"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "npm"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "ESLint",
+      b: "jQuery",
+      c: "RequireJS",
+    },
+    correctAnswer: "ESLint"
+  }
+];
 
-for(var i = 0; i < Questions.length; i++){
-  clicked = document.getElementById(e.id).value;
+function buildQuiz() {
+  // setTime();
+  if (quesCount >= Questions.length){
+    var result = document.getElementById("content");
+    result.innerHTML = "You scored: " + score + "out of " + Questions.length;
+  };
+  setTime();
+  // currQuestion = Questions[quesCount].question;
+  var content = document.getElementById("content");
+  var chA = Questions[quesCount].answers.a;
+  var chB = Questions[quesCount].answers.b;
+  var chC = Questions[quesCount].answers.c;
+  answer = Questions[quesCount].correctAnswer;
+  content.innerHTML = "";
+  content.innerHTML = "<h1>" + Questions[quesCount].question + "<br/></h1>";
+  content.innerHTML += "<div><button id = 'button1' onClick=checkAnswer(this) value='" + chA + "'>" + chA + "</button></div><br>";
+  content.innerHTML += "<div><button id = 'button2' onClick=checkAnswer(this) value='" + chB + "'>" + chB + "</button></div><br>";
+  content.innerHTML += "<div><button id = 'button3' onClick=checkAnswer(this) value='" + chC + "'>" + chC + "</button></div><br>";
+  quesCount++;
+}
+
+function checkAnswer(e) {
+  
+  var clicked = document.getElementById(e.id).value;
   console.log(clicked);
   console.log(answer);
-  if(clicked === answer){
+  if (clicked === answer) {
     score++
   };
   console.log(score);
   buildQuiz();
-};
-showResults();
-}
-
-function showResults(){
-  console.log(Questions.length)
-  var content = document.getElementById("content");
-  content.innerHTML="You scored: " + score + "out of " + Questions.length ;
 }
 
 var secondsLeft = 10;
 
 function setTime() {
-  var timerInterval = setInterval(function() {
-  secondsLeft--;
-  var content = document.getElementById("content");
-  var timeEl = document.createElement("div");
-  timeEl.textContent = secondsLeft + " seconds left till Quiz Ends.";
-  content.innerHTML=timeEl;
-  if(secondsLeft === 0) {
-    clearInterval(timerInterval);
-    sendMessage();
-  }
+  var timerInterval = window.setInterval(function () {
+    secondsLeft--;
+    var Elspt = document.getElementById("time");
+    Elspt.innerHTML += secondsLeft + " seconds left till Quiz Ends.";
+ 
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
 
   }, 1000);
 }
 
 function sendMessage() {
   var content = document.getElementById("content");
-  content.innerHTML="Times Up!";
+  content.innerHTML = "Times Up!";
+  var result = document.getElementById("content");
+  result.innerHTML = "You scored: " + score + "out of " + Questions.length;
 }
 
 startquiz.addEventListener('click', buildQuiz);
